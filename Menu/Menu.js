d14c3e06d74a9fbd7df6c10140e94dc882fa2ed7
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-04-15 16:04:52 -0700

    created menu but doesnt display

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,6 +9,37 @@ let menuItems = [
   'Log Out'
 ];
 
+/*
+  menutItems.forEach((item) => {
+    let listItem = createListItem(item);
+    ul.appendChild(listItem);
+  })
+
+
+*/
+
+// function createListItem(text) {
+//   let listItem = document.createElement('li');
+//   listItem.textContent = text;
+// }
+
+// function createMenu(arr) {
+//   let div = document.createElement('div');
+//   div.classList.add('menu');
+  
+//   for(let i= 0; i < arr.length(); i++)
+//   {
+//     let items = createListItem(arr[i]);
+//     ul.appendChild(items);
+//   } 
+//   div.appendChild(ul);
+// }
+
+// let ul = document.createElement('ul');
+// let menu = document.querySelector('.menu-button');
+
+// createMenu(menuItems);
+
 /* 
 
   Step 1: Write a function that will create a menu component as seen below:
@@ -33,3 +64,50 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+// let menuButton = document.querySelector(".menu-button");
+// menuButton.addEventListener('click', (event) =>{
+//   console.log("something is happening");
+// })
+
+const menu = document.createElement('div');
+menu.classList.add('menu');
+
+const menuBar = document.createElement('ul');
+menuBar.classList.add('menu-bar');
+
+const menuContent = document.createElement('li');
+menuContent.classList.add('menu-content');
+
+
+let menuButton = document.querySelector('.menu-button');
+
+//title
+const buttonTitle = document.createElement('h3');
+buttonTitle.textContent = "Title of bar";
+
+//buttons
+const buttonsContainer = document.createElement('div');
+buttonsContainer.classList.add('buttons-container');
+buttonsContainer.textContent = "content of menu";
+
+const openButton = document.createElement('button');
+const closeButton = document.createElement('button');
+openButton.classList.add('open-button');
+openButton.textContent = "\u25bc";
+
+closeButton.classList.add('close-button');
+closeButton.textContent = "\u25b2";
+
+//appending
+menuButton.appendChild(menu);
+menu.appendChild(menuBar);
+menuBar.appendChild(buttonTitle);
+menuBar.appendChild(buttonsContainer);
+menuBar.appendChild(menuContent);
+
+//appending buttons
+buttonsContainer.appendChild(openButton);
+buttonsContainer.appendChild(closeButton);
+
+console.log(menu);
